ae6a4d5ed4eb2a2bddde5ae51ac8b3a9825fb020
chungdev99 <chungtranphuoc67@gmail.com>
2023-03-22 12:45:49 +0700

    first commit

--- README.md
+++ README.md
@@ -1,24 +1,27 @@
 ## Introduction
 ***
-- This is a Website TodoList (CRUD) with FE: (ReactJs, Material UI) + BE: Java
+- This is a Website TodoList (CRUD) to manage the list 
+  - FE: ReactJs, Material UI, JavaScript, React Hook, React-Hook-Form, React-Router-Dom.
+  - API: Json_Server.
+
+- Link Demo:... ( improving further ) 
 
 ## Features
 ***
-- Responsive.
-- CRUD. 
+- Main function CRUD
 - TodoList
-    - Add Todo
-    - Check Todo (complete or incomplete)
-    - Edit Todo
-    - Remove Todo
-- StudentList
-    - Search student
-    - Add student
-    - Update student
-    - Remove student
+    - Add Todo.
+    - Check Todo ( Complete or Incomplete ).
+    - Edit Todo.
+    - Remove Todo.
+- StudentList ( Call API )
+    - Search student.
+    - Add student.
+    - Update student.
+    - Remove student.
 - ....
 
-## Page
+
 
 
 
--- src/feature/components/UpdateStudents/Update.jsx
+++ src/feature/components/UpdateStudents/Update.jsx
@@ -6,16 +6,19 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { Box } from '@mui/material';
+import { Box, IconButton } from '@mui/material';
 import { Container } from '@mui/system';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import RestartAltIcon from '@mui/icons-material/RestartAlt';
+import { Controller, useForm } from 'react-hook-form';
+import { Edit } from '@mui/icons-material';
 
 
 function Update() {
   const [studentList, setStudentList] = useState([]);
+  const { control, reset, handleSubmit } = useForm();
 
   useEffect(() => {
     async function fetchUpdate() {
@@ -27,23 +30,23 @@ function Update() {
     fetchUpdate();
   }, []);
 
-  const hdlUpdateBtn = async (event) => {
-    event.preventDefault();
-    let idinp = document.getElementById('idInp').value;
-    let nameinp = document.getElementById('nameInp').value;
-    let emailinp = document.getElementById('emailInp').value;
-    let genderinp = document.getElementById('genderInp').value;
-    let teamsinp = document.getElementById('teamInp').value;
-    const data = {
-      id: idinp,
-      name: nameinp,
-      teams: teamsinp,
-      email: emailinp,
-      gender: genderinp,
-    };
+  const hdlUpdateBtn = async (data) => {
+    // event.preventDefault();
+    // let idinp = document.getElementById('idInp').value;
+    // let nameinp = document.getElementById('nameInp').value;
+    // let emailinp = document.getElementById('emailInp').value;
+    // let genderinp = document.getElementById('genderInp').value;
+    // let teamsinp = document.getElementById('teamInp').value;
+    // const data = {
+    //   id: idinp,
+    //   name: nameinp,
+    //   teams: teamsinp,
+    //   email: emailinp,
+    //   gender: genderinp,
+    // };
     console.log(data);
 
-    const requestUrl = `http://localhost:4000/api/categories/${idinp}`
+    const requestUrl = `http://localhost:4000/api/categories/${data.id}`
     const response = await fetch(requestUrl, {
       method: "put",
       headers: {
@@ -64,13 +67,29 @@ function Update() {
   };
 
   const hdlResetBtn = (event) => {
-    event.preventDefault();
-    document.getElementById('idInp').value = '';
-    document.getElementById('nameInp').value = '';
-    document.getElementById('emailInp').value = '';
-    document.getElementById('genderInp').value = '';
-    document.getElementById('teamInp').value = '';
+    // event.preventDefault();
+    // document.getElementById('idInp').value = '';
+    // document.getElementById('nameInp').value = '';
+    // document.getElementById('emailInp').value = '';
+    // document.getElementById('genderInp').value = '';
+    // document.getElementById('teamInp').value = '';
+reset({
+  id:'',
+  name: '',
+  email: '',
+  gender:'',
+  teams:'',
+})
   };
+
+  // const onSubmit = data => {
+  //   console.log(data, "data")
+  // }
+
+  const handleSetValue = index => {
+    console.log(studentList[index])
+    reset(studentList[index])
+  }
 
   return (
     <div className="wrapper">
@@ -87,19 +106,22 @@ function Update() {
               <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 <TableHead>
                   <TableRow>
+                  {/* <TableCell>idex</TableCell> */}
                     <TableCell>id</TableCell>
                     <TableCell align="left">name</TableCell>
                     <TableCell align="left">email&nbsp;</TableCell>
                     <TableCell align="left">gender&nbsp;</TableCell>
                     <TableCell align="left">teams&nbsp;</TableCell>
+                    <TableCell align="left">actions</TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {studentList.map((list) => (
+                  {studentList.map((list, index) => (
                     <TableRow
                       key={list.id}
                       sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                     >
+                      {/* <TableCell component="th" scope="row">{index}</TableCell> */}
                       <TableCell component="th" scope="row">
                         {list.id}
                       </TableCell>
@@ -107,6 +129,7 @@ function Update() {
                       <TableCell align="left">{list.email}</TableCell>
                       <TableCell align="left">{list.gender}</TableCell>
                       <TableCell align="left">{list.teams}</TableCell>
+                      <TableCell align="left"><IconButton onClick={() => handleSetValue(index)}><Edit /></IconButton></TableCell>
                     </TableRow>
                   ))}
                 </TableBody>
@@ -119,34 +142,72 @@ function Update() {
         </div>
         <form onSubmit={hdlUpdateBtn}>
           <div className="form-group">
-            <TextField id="idInp" label="id" variant="outlined" size="small" />
+            <Controller
+              name="id"
+              control={control}
+              defaultValue=""
+              render={({ field: { value, onChange } }) => <TextField onChange={onChange} value={value} label="id" variant="outlined" size="small" />}
+            />
           </div>
+
           <br></br>
+
           <div className="form-group">
-            <TextField id="nameInp" label="name" variant="outlined" size="small" />
+            <Controller
+              name="name"
+              control={control}
+              defaultValue=""
+              render={({ field: { value, onChange } }) => <TextField onChange={onChange} value={value} label="name" variant="outlined" size="small" />}
+            />
+
           </div>
+
           <br></br>
+
           <div className="form-group">
-            <TextField id="emailInp" label="email" variant="outlined" size="small" />
+            <Controller
+              name="email"
+              control={control}
+              defaultValue=""
+              render={({ field: { value, onChange } }) => <TextField onChange={onChange} value={value} label="email" variant="outlined" size="small" />}
+            />
+
           </div>
+
           <br></br>
+
           <div className="form-group">
-            <TextField id="genderInp" label="gender" variant="outlined" size="small" />
+            <Controller
+              name="gender"
+              control={control}
+              defaultValue=""
+              render={({ field: { value, onChange } }) => <TextField onChange={onChange} value={value} label="gender" variant="outlined" size="small" />}
+            />
           </div>
+
           <br></br>
+
           <div className="form-group">
-            <TextField id="teamInp" label="team" variant="outlined" size="small" />
-          </div>
+            <Controller
+              name="teams"
+              control={control}
+              defaultValue=""
+              render={({ field: { value, onChange } }) => <TextField onChange={onChange} value={value} label="team" variant="outlined" size="small" />}
+            />       </div>
+
           <br></br>
           <div className="form-group">
             <Stack direction="row" spacing={1}>
               <Button
                 variant="contained"
                 className="btn btn-success"
-                type="submit"
+                // type="submit"
+                // onClick={() => reset({ id: 123 })}
+                onClick={handleSubmit(hdlUpdateBtn)}
               >
                 UPDATE
               </Button>
+              {/* <Button onClick={handleSubmit(onSubmit)}>Submit</Button> */}
               <Button type="reset" onClick={hdlResetBtn}>
                 <RestartAltIcon />
               </Button>
